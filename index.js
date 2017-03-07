a52c7bf383ba53f1d0ccd5140a1eebf038839a03
Aman Dureja <dureja.aman.97@gmail.com>
2017-03-06 23:24:28 -0500

    fix navbar on mobile

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var navLinksClassList = document.getElementById('mobile-nav-links').classList;
+var mobileNavLinks = document.getElementsByClassName('nav-link-mobile');
+
 document.getElementById('mobile-nav-enable').onclick = function() {
-  var navLinksClassList = document.getElementById('mobile-nav-links').classList;
   if (navLinksClassList.contains('visible')) {
     navLinksClassList.remove('visible');
   }
@@ -7,3 +9,9 @@ document.getElementById('mobile-nav-enable').onclick = function() {
     navLinksClassList.add('visible');
   }
 }
+
+Array.prototype.forEach.call(mobileNavLinks, function(link) {
+  link.onclick = function() {
+    navLinksClassList.remove('visible');
+  }
+});
