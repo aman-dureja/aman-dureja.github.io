55effec6f91aaf800abf0673c45ed3e0d7e492ad
Aman Dureja <dureja.aman.97@gmail.com>
2017-12-30 19:14:14 -0500

    New design

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var navLinksClassList = document.getElementById('mobile-nav-links').classList;
-var mobileNavLinks = document.getElementsByClassName('nav-link-mobile');
-
-document.getElementById('mobile-nav-enable').onclick = function() {
-  if (navLinksClassList.contains('visible')) {
-    navLinksClassList.remove('visible');
-  }
-  else {
-    navLinksClassList.add('visible');
-  }
-}
-
-Array.prototype.forEach.call(mobileNavLinks, function(link) {
-  link.onclick = function() {
-    navLinksClassList.remove('visible');
-  }
-});
+var navLinksClassList=document.getElementById("mobile-nav-links").classList,mobileNavLinks=document.getElementsByClassName("nav-link-mobile");document.getElementById("mobile-nav-enable").onclick=function(){navLinksClassList.contains("visible")?navLinksClassList.remove("visible"):navLinksClassList.add("visible")},Array.prototype.forEach.call(mobileNavLinks,function(i){i.onclick=function(){navLinksClassList.remove("visible")}});
